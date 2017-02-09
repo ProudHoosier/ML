f893af13c4d1d7c2e4be8b552b16ea9971eafe76
Shashi <shashank@umail.iu.edu>
2017-02-08 22:30:08 -0500

    Update EM.py

--- EM.py
+++ EM.py
@@ -2,40 +2,41 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
-x = np.loadtxt(data1.txt)
+x = np.loadtxt('data1.txt')
 
 # number of mixtures
-M = 3 
+m = 3
+n = m-1
 # likelihood of the mixture
-alpha = np.ones(M)*(1/M)
+alpha = np.ones(m)*(1/m)
 # Intitialize means randomly
-mu = np.random.random(M)*50 
+mu = np.random.random(m)*50 
 # Intitialize sd randomly
-sigma = np.ones(M)*100.0 
+sigma = np.ones(m)*100.0 
 
-weight = np.zeros((M, len(x)))
+weight = np.zeros((m, len(x)))
+
+def expectation():
+	weight[n] = weight[n] = alpha[n] * mlab.normpdf(x, mu[n], sigma[n])
+
+def maximization():
+	mu = np.sum(weight*x, axis=1)/np.sum(weight, axis=1)
+	sigma = np.sqrt(np.sum(weight * pow(x - mu[:,np.newaxis],2), axis=1) / np.sum(weight, axis=1))
+
+	#calculate new class priors
+	alpha = np.sum(weight, axis=1)/len(x)
 
 for i in range(50):
 	print('alpha:', alpha, 'mu:', mu, 'sigma', sigma)
 	L = 0
-	for j in range(M):
+	for j in range(m):
 		expectation()
-	
 	maximization()
+    
 	for k in range(len(x)):
-		L += log(np.sum(weight, axis=1))
+		L += np.log(np.sum(weight, axis=1))
 	print('Log likelihood:', L)
-	
-	
-def expectation():
-	weight[m] = weight[m] = alpha[m] * mlab.normpdf(x, mu[m], sigma[m])
-	
-	
-def maximization():
-	mu = np.sum(weight*x, axis=1)/np.sum(weight, axis=1)	
-	sigma = np.sqrt(np.sum(weight * pow(x - mu[:,np.newaxis],2), axis=1) / np.sum(weight, axis=1))
-	
-	#calculate new class priors
-	alpha = np.sum(weight, axis=1)/len(x)
-	
+
+
+
 
